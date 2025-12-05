c18f9cc905da5dfa2c465404fe1eb57526580037
akaMiWP <bangkokappdevelopment@gmail.com>
2025-12-05 14:57:05 +0700

    feat: decorated history screen

--- FocusTrackerClient/FocusTrackerClient/Screens/History/HistoryView.swift
+++ FocusTrackerClient/FocusTrackerClient/Screens/History/HistoryView.swift
@@ -29,7 +29,7 @@ struct HistoryView: View {
                                     HStack {
                                         Text("duration:")
                                         Spacer()
-                                        Text(String(session[keyPath: \.focusItem.duration]))
+                                        Text("\(String(session[keyPath: \.focusItem.duration])) minutes")
                                     }
                                     .font(.footnote)
                                     .foregroundStyle(.gray)
